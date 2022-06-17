46bbeabdf1482edd359486ff0e4cc498f4af0c93
Priyanshu Singh <singhpriyanshu957@gmail.com>
2022-06-18 02:33:46 +0530

    added multiply.cpp

---
+++ multiply/multiply/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  multiply
+//
+//  Created by Priyanshu Singh on 18/06/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
